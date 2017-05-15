5fa29e00daf56d74a641eb5121fb3344934420b1
AnikethReddy <waraniketh1@gmail.com>
2017-05-15 15:14:52 +0530

    reading from file

--- War3.scala
+++ War3.scala
@@ -5,11 +5,14 @@
  * Time: 12:34 PM
  * To change this template use File | Settings | File Templates.
  */
+
+import scala.io.Source
 object War3{
   def main(args:Array[String]){
-    var eee=Array("war1","war2","war3w")
-    println(eee.last.equals("swar3w") )
-    eee.map(_+"warmama").foreach{println}
+    var opening=Source.fromFile("/home/hduser/file1.txt").getLines()
+    var tot=opening.filter((_.equals("amazed agonize"))).flatMap(_.split(" ")).map(word=>(word,10))
+    var tot1=Array(12,10,10)
+    println(tot1.reduceLeft(_+_))
 
   }
 }
